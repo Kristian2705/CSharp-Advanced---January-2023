d53855936dbf075936461e6079140666cd7f9837
Kristian2705 <k_petrov27@abv.bg>
2023-02-04 12:45:34 +0200

    Added Generics Lab

---
+++ 08.Generics-Lab/1.BoxOfT/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> box;
+        private int count;
+
+        public Box()
+        {
+            box = new List<T>();
+        }
+
+        public int Count { get { return count; } }
+
+        public void Add(T element)
+        {
+            count++;
+            box.Insert(0, element);
+        }
+
+        public T Remove()
+        {
+            count--;
+            T elementToRemove = box[0];
+            box.RemoveAt(0);
+            return elementToRemove;
+        }
+    }
+}
---
+++ 08.Generics-Lab/2.GenericArrayCreator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+            Console.WriteLine(string.Join(" ", strings));
+            Console.WriteLine(string.Join(" ", integers));
+        }
+    }
+}
