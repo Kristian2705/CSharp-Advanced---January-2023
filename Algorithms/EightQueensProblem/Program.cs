41d51de155d76c73ee42209db784697a94ef53bc
Kristian2705 <k_petrov27@abv.bg>
2023-02-15 17:41:28 +0200

    Added EightQueensProblem and FindingAllPathsInAMaze

---
+++ Algorithms/EightQueensProblem/Program.cs
@@ -0,0 +1,85 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[,] board = new int[n, n];
+
+Console.WriteLine(FindQueens(board, 0));
+
+int FindQueens(int[,] board, int row)
+{
+    if (row == board.GetLength(0))
+    {
+        PrintBoard(board);
+        return 1;
+    }
+    int foundQueens = 0;
+    for (int col = 0; col < board.GetLength(1); col++)
+    {
+        if (IsSafe(board, row, col))
+        {
+            board[row, col] = 1;
+            foundQueens += FindQueens(board, row + 1);
+            board[row, col] = 0;
+        }
+    }
+    return foundQueens;
+}
+
+bool IsSafe(int[,] board, int row, int col)
+{
+    for (int i = 0; i < board.GetLength(0); i++)
+    {
+        if (row - i >= 0 && board[row - i, col] == 1)
+        {
+            return false;
+        }
+        if (row + i < board.GetLength(0) && board[row + i, col] == 1)
+        {
+            return false;
+        }
+        if (col - i >= 0 && board[row, col - i] == 1)
+        {
+            return false;
+        }
+        if (col + i < board.GetLength(1) && board[row, col + i] == 1)
+        {
+            return false;
+        }
+        if (row - i >= 0 && col - i >= 0 && board[row - i, col - i] == 1)
+        {
+            return false;
+        }
+        if (row - i >= 0 && col + i < board.GetLength(1) && board[row - i, col + i] == 1)
+        {
+            return false;
+        }
+        if (row + i < board.GetLength(0) && col - i >= 0 && board[row + i, col - i] == 1)
+        {
+            return false;
+        }
+        if (row + i < board.GetLength(0) && col + i < board.GetLength(1) && board[row + i, col + i] == 1)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void PrintBoard(int[,] board)
+{
+    for (int i = 0; i < board.GetLength(0); i++)
+    {
+        for (int j = 0; j < board.GetLength(1); j++)
+        {
+            if (board[i, j] != 0)
+            {
+                Console.Write("Q");
+            }
+            else
+            {
+                Console.Write("-");
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
---
+++ Algorithms/FindingAllPathsInAMaze/Program.cs
@@ -0,0 +1,51 @@
+﻿string[] maze = new string[]
+{
+    "010001",
+    "01010E",
+    "010100",
+    "000000"
+};
+const int startRow = 0;
+const int startCol = 0;
+PrintMaze(maze);
+Console.WriteLine();
+Console.WriteLine("All possible path starting from [0][0]:");
+FindPaths(startRow, startCol, maze, new bool[maze.GetLength(0), maze[0].Length], "");
+
+void FindPaths(int row, int col, string[] maze, bool[,] visitedPlaces, string path)
+{
+    if (maze[row][col] == 'E')
+    {
+        Console.WriteLine(path);
+        return;
+    }
+    visitedPlaces[row, col] = true;
+    if(IsSafe(row - 1, col, maze, visitedPlaces))
+    {
+        FindPaths(row - 1, col, maze, visitedPlaces, path + "U");
+    }
+    if (IsSafe(row + 1, col, maze, visitedPlaces))
+    {
+        FindPaths(row + 1, col, maze, visitedPlaces, path + "D");
+    }
+    if (IsSafe(row, col - 1, maze, visitedPlaces))
+    {
+        FindPaths(row, col - 1, maze, visitedPlaces, path + "L");
+    }
+    if (IsSafe(row, col + 1, maze, visitedPlaces))
+    {
+        FindPaths(row, col + 1, maze, visitedPlaces, path + "R");
+    }
+    visitedPlaces[row, col] = false;
+}
+
+bool IsSafe(int row, int col, string[] maze, bool[,] visitedPlaces)
+    => row >= 0 && col >= 0 && row < maze.GetLength(0) && col < maze[0].Length && !visitedPlaces[row, col] && maze[row][col] != '1';
+
+void PrintMaze(string[] maze)
+{
+    for(int i = 0; i < maze.Length; i++)
+    {
+        Console.WriteLine(maze[i]);
+    }
+}
